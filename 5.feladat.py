f598cec3457adff567e01a96d060e843ff764ae1
Kpatyi <116498778+Kpatyi@users.noreply.github.com>
2022-11-13 13:42:00 +0100

    verzió_1

---
+++ 5.feladat.py
@@ -0,0 +1,5 @@
+#Kérjünk be két egész számot és írjuk ki az összegüket
+szam1=int(input("Kérem az első számot:"))
+szam2=int(input("Kérem a második számot:"))
+osszeg=szam1+szam2
+print("A két szám összege:",osszeg)
